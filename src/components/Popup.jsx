ceea77e197d9bd97c3708ff90447652b470156f7
Adam <adamboman1@hotmail.com>
2023-05-28 11:38:23 -0600

    lower tokens for testing

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -6,7 +6,6 @@ const Popup = ({ response, isVisible, onClose, onSave }) => {
             <Modal.Header />
             <Modal.Body>
                 <div className="text-center">
-                    <G className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
                     <h3 className="mb-5 text-lg font-normal text-black dark:text-gray-400">
                         Lesson Plan
                     </h3>
