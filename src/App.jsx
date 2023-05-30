73e79429055974801265f07cf8acd428afa6ec82
ajboman <89932630+ajboman@users.noreply.github.com>
2023-05-30 11:52:49 -0600

    Added in Anonymous User Authentication With Firebase (#15)
    * anonymous user auth
    
    * remove console.log

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
+import UserContext from './services/UserContext';  
 
+import { getAuth, signInAnonymously } from "firebase/auth";
+import { app } from './services/Firebase';
 import Navbar from './components/Navbar';
 import Info from './components/Info';
 import Form from './components/Form';
@@ -19,12 +22,27 @@ const RedirectToPlan = () => {
 
 const App = () => {
   const [allLessons, setAllLessons] = useState([]);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     const storedLessons = JSON.parse(localStorage.getItem('lessons'));
     if (storedLessons) {
       setAllLessons(storedLessons);
     }
+  }, []);
+
+  useEffect(() => {
+    const auth = getAuth(app);
+
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      if (!user) {
+        signInAnonymously(auth).catch(error => console.error(error));
+      } else {
+        setUser(user);
+      }
+    });
+
+    return () => unsubscribe();
   }, []);
 
   const saveLesson = (lesson) => {
@@ -40,19 +58,22 @@ const App = () => {
     localStorage.setItem('lessons', JSON.stringify(updatedLessons));
   };
 
-  
+
   return (
-    <Router>
-      <main>
-        <div className='main' />
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<RedirectToPlan />} />
-          <Route path="/plan" element={<><Info /><Form saveLesson={saveLesson} /></>} />
-          <Route path="/lessons" element={<Lessons allLessons={allLessons} onDeleteLesson={deleteLesson} />} />
-        </Routes>
-      </main>
-    </Router>
+    <UserContext.Provider value={user}>
+      <Router>
+        <main>
+          <div className='main' />
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<RedirectToPlan />} />
+            <Route path="/plan" element={<><Info /><Form saveLesson={saveLesson} /></>} />
+            <Route path="/lessons" element={<Lessons allLessons={allLessons} onDeleteLesson={deleteLesson} />} />
+          </Routes>
+        </main>
+      </Router>
+    </UserContext.Provider>
+
   );
 }
 
