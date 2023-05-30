2cbf8b03f75b8f8b65588d30a113fd74bdc6db74
Adam <adamboman1@hotmail.com>
2023-05-30 16:18:23 -0600

    indents and buttons are links

--- src/__tests__/Navbar.test.js
+++ src/__tests__/Navbar.test.js
@@ -3,31 +3,31 @@ import { render, screen } from '@testing-library/react';
 import Navbar from '../components/Navbar';
 
 describe('Navbar Component', () => {
-    beforeEach(() => {
-        render(
-            <Router>
-                <Navbar />
-            </Router>
-        );
-    });
+	beforeEach(() => {
+		render(
+			<Router>
+				<Navbar />
+			</Router>
+		);
+	});
 
-    it('renders without crashing', () => {
-        const navbarElement = screen.getByText(/Lesson Planner/i);
-        expect(navbarElement).toBeInTheDocument();
-    });
+	it('renders without crashing', () => {
+		const navbarElement = screen.getByText(/Lesson Planner/i);
+		expect(navbarElement).toBeInTheDocument();
+	});
 
-    it('renders links correctly', () => {
-        const planLinkElement = screen.getByRole('link', { name: /Plan/i });
-        expect(planLinkElement).toBeInTheDocument();
-        expect(planLinkElement).toHaveAttribute('href', '/plan');
+	it('renders links correctly', () => {
+		const planLinkElement = screen.getByRole('link', { name: /Plan/i });
+		expect(planLinkElement).toBeInTheDocument();
+		expect(planLinkElement).toHaveAttribute('href', '/plan');
 
-        const lessonsLinkElement = screen.getByRole('link', { name: /Lessons/i });
-        expect(lessonsLinkElement).toBeInTheDocument();
-        expect(lessonsLinkElement).toHaveAttribute('href', '/lessons');
-    });
+		const lessonsLinkElement = screen.getByRole('link', { name: /Lessons/i });
+		expect(lessonsLinkElement).toBeInTheDocument();
+		expect(lessonsLinkElement).toHaveAttribute('href', '/lessons');
+	});
 
-    it('renders GitHub button correctly', () => {
-        const githubButtonElement = screen.getByText(/GitHub/i);
-        expect(githubButtonElement).toBeInTheDocument();
-    });
+	it('renders GitHub button correctly', () => {
+		const githubButtonElement = screen.getByText(/GitHub/i);
+		expect(githubButtonElement).toBeInTheDocument();
+	});
 });
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,43 +4,44 @@ import { useContext } from 'react';
 import UserContext from '../services/UserContext';
 
 const Navbar = ({ openLogin }) => {
-    const user = useContext(UserContext);
-    const isAnonymous = user ? user.isAnonymous : true;
+	const user = useContext(UserContext);
+	const isAnonymous = user ? user.isAnonymous : true;
 
-    return (
-        <header className="w-full flex justify-center items-center flex-col p-5">
-            <nav className='flex justify-between items-center w-full mb-10 pt-3'>
-                <div className="text-2xl text-black">Lesson Planner</div>
-                <div className="link-buttons-container">
-                    <Button className="link-button">
-                        <Link to="/plan" className='text-white'>Plan</Link>
-                    </Button>
-                    <Button className="link-button">
-                        <Link to="/lessons" className='text-white'>Lessons</Link>
-                    </Button>
-                </div>
-                {isAnonymous ? (
-                    <Button 
-                        className="link-button"
-                        onClick={openLogin}
-                    >
-                        Login
-                    </Button>
-                ) : (
-                    <Button className="link-button">
-                        <Link to="/profile" className='text-white'>Profile</Link>
-                    </Button>
-                )}
-                <Button
-                    type='button'
-                    onClick={() => window.open('https://github.com/ajboman/LessonPlanner')}
-                    className='black_btn'
-                >
-                    GitHub
-                </Button>
-            </nav>
-        </header>
-    )
+	return (
+		<header className="w-full flex justify-center items-center flex-col p-5">
+			<nav className='flex justify-between items-center w-full mb-10 pt-3'>
+				<div className="text-2xl text-black">Lesson Planner</div>
+				<div className="link-buttons-container">
+					<Link to="/plan" className='text-white'>
+						<Button className="link-button">Plan</Button>
+					</Link>
+					<Link to="/lessons" className='text-white'>
+						<Button className="link-button"> Lessons</Button>
+					</Link>
+
+				</div>
+				{isAnonymous ? (
+					<Button
+						className="link-button"
+						onClick={openLogin}
+					>
+						Login
+					</Button>
+				) : (
+					<Link to="/profile" className='text-white'>
+						<Button className="link-button"> Profile </Button>
+					</Link>
+				)}
+				<Button
+					type='button'
+					onClick={() => window.open('https://github.com/ajboman/LessonPlanner')}
+					className='black_btn'
+				>
+					GitHub
+				</Button>
+			</nav>
+		</header>
+	)
 }
 
 export default Navbar;
